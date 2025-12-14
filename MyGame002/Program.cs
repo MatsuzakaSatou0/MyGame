3770af501c896e2a26226e339f0f0ec67ace45f8
Satou Matsuzaka <106461491+MatsuzakaSatou0@users.noreply.github.com>
2025-12-14 17:06:20 +0900

    ベース

--- MyGame002/Program.cs
+++ MyGame002/Program.cs
@@ -1,28 +1,5 @@
 ﻿using MyGame002;
-using MyGame002.GameProgram;
-using MyGame002.GameProgram.Developer;
-using MyGame002.GameProgram.Game;
-using MyGame002.GameProgram.Game.G0;
-using MyGame002.GameProgram.Game.G1;
-using MyGame002.GameProgram.Game.G2;
-using MyGame002.GameProgram.GameLauncher;
-using MyGame002.GameProgram.Program;
-using MyGame002.GameProgram.Yukari;
 using System.IO;
-var launcher = new GameLauncher();
-//launcher.SkipLogo();
-MyGame002.Game1.GetInstance().RegisterGame(launcher);
-
-//プログラムリスト
-MyGame002.Game1.GetInstance().GetProgramManager().AddProgram(new GameProgramInfo("HelloWorld", "1.0.0", new Game000()));
-MyGame002.Game1.GetInstance().GetProgramManager().AddProgram(new GameProgramInfo("Chaser", "1.0.0", new Game001()));
-MyGame002.Game1.GetInstance().GetProgramManager().AddProgram(new GameProgramInfo("Predictor", "1.0.0", new Game002()));
-
-//関係のないプログラムリスト
-
-MyGame002.Game1.GetInstance().GetProgramManager().AddProgram(new GameProgramInfo("DevMenu", "デバッグ用", new DevMenu()));
-MyGame002.Game1.GetInstance().GetProgramManager().AddProgram(new GameProgramInfo("3dGame", "開発中", new My3dGame()));
-MyGame002.Game1.GetInstance().GetProgramManager().AddProgram(new GameProgramInfo("Yukari", "開発中", new Yukari()));
-
+//MyGame002.Game1.GetInstance().RegisterGame(launcher);
 
 MyGame002.Game1.GetInstance().Run();
