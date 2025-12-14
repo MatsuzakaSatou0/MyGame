e254319af6e126fbb8aebbe158c259eae357906d
Satou Matsuzaka <106461491+MatsuzakaSatou0@users.noreply.github.com>
2025-12-14 20:40:29 +0900

    プログラムの外部化

--- MyGame002/Program.cs
+++ MyGame002/Program.cs
@@ -1,6 +1,50 @@
 ﻿using MyGame002;
-using MyGame002.GameProgram;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-MyGame002.Game1.GetInstance().RegisterGame(new AudioTest());
+using System.Reflection;
 
-MyGame002.Game1.GetInstance().Run();
+if (!Directory.Exists("Programs"))
+{
+    //強制終了
+    goto End;
+}
+else
+{
+    //読み込み
+    goto Load;
+}
+
+Load:
+{
+    //DLLのリスト
+    List<Assembly> programes = new List<Assembly>();
+    //Programsフォルダー内のdll読み込み。
+    foreach (string path in Directory.GetFiles("Programs"))
+    {
+        //dllを読み込み追加
+        programes.Add(Assembly.LoadFile(Path.GetFullPath(path)));
+    }
+    foreach (Assembly program in programes)
+    {
+        //dllの中からentryのtypeファイルを取得。
+        var entry = program.GetTypes();
+        foreach(Type t in entry)
+        {
+            var methods = t.GetMethods();
+            foreach (MethodInfo info in methods)
+            {
+                if (info.Name == "main")
+                {
+                    var main = info.Invoke(null, null);
+                }
+            }
+        }
+    }
+    Game1.GetInstance().Run();
+}
+End:
+{
+
+}
